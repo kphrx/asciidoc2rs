61734ed762e000f6ac18397bde1fd10bb05cd9a0
kPherox <admin@mail.kr-kp.com>
2023-04-06 19:21:50 +0900

    fix: change parse not vec, add doctype enum

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,32 @@ pub trait CompoundBlock: Block {
 pub trait Block: DynClone {}
 clone_trait_object!(Block);
 
+pub trait Inline: DynClone {
+    fn iter(&self) -> Box<dyn Iterator<Item = Box<dyn Inline>>>;
+}
+clone_trait_object!(Inline);
+
 #[derive(Clone)]
 pub struct Document {
     blocks: Vec<Box<dyn Block>>,
+    doctype: Doctype,
+}
+
+impl Document {
+    fn new(doctype: Doctype) -> Self {
+        let blocks = Vec::with_capacity(1);
+
+        Document { blocks, doctype }
+    }
+
+    fn title(self) -> Option<String> {
+        match self.doctype {
+            Doctype::Manpage => panic!("require document title"),
+            _ => false,
+        };
+
+        None
+    }
 }
 
 impl Block for Document {}
@@ -20,19 +43,33 @@ impl CompoundBlock for Document {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum Doctype {
+    Article,
+    Book,
+    Manpage,
+}
+
 pub struct Parser<'input> {
     text: &'input str,
+    doctype: Doctype,
 }
 
 impl<'input> Parser<'input> {
     pub fn new(text: &'input str) -> Self {
-        Self { text }
+        Self::new_with_doctype(text, Doctype::Article)
     }
 
-    pub fn parse(self) -> Vec<Document> {
-        let documents = Vec::with_capacity(0);
+    pub fn new_with_doctype(text: &'input str, doctype: Doctype) -> Self {
+        Self { text, doctype }
+    }
 
-        documents
+    pub fn parse(self) -> Document {
+        Document::new(self.doctype)
+    }
+
+    pub fn parse_inline(self) -> Vec<Box<dyn Inline>> {
+        Vec::with_capacity(1)
     }
 }
 
@@ -44,10 +81,9 @@ mod tests {
     fn paragraph_block() {
         let parser = Parser::new("Paragraphs don't require any special markup in AsciiDoc.\nA paragraph is just one or more lines of consecutive text.\n\nTo begin a new paragraph, separate it by at least one empty line from the previous paragraph or block.");
 
-        let documents = parser.parse();
-        assert_eq!(1, documents.len());
-
-        let document = documents[0].clone();
+        let document = parser.parse();
         assert_eq!(2, document.iter().count());
+        assert_eq!(None, document.clone().title());
+
     }
 }
