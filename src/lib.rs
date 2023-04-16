b9f2be3aef93e8b44b0c1306abc23eb21c611cc8
kPherox <admin@mail.kr-kp.com>
2023-04-17 01:18:43 +0900

    ci: code coverage

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! # asciidoc2rs
 //! ![TCK passed count](https://img.shields.io/endpoint?style=flat-square&url=https://gist.githubusercontent.com/kphrx/e795d8d23466d049a08e03c23301e996/raw/asciidoc2rs-passed.json)
 //! ![TCK failed count](https://img.shields.io/endpoint?style=flat-square&url=https://gist.githubusercontent.com/kphrx/e795d8d23466d049a08e03c23301e996/raw/asciidoc2rs-failed.json)
+//! ![Codecov](https://img.shields.io/codecov/c/gh/kphrx/asciidoc2rs?style=flat-square)
 //!
 //! A parser for AsciiDoc
 
